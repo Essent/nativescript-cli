7a6104870715ad81408672d77ea4e0f87f35c3de
spike1292 <spike1292@gmail.com>
2017-04-10 20:32:24 +0200

    manual set code sign identity

--- lib/services/ios-project-service.ts
+++ lib/services/ios-project-service.ts
@@ -372,7 +372,7 @@ export class IOSProjectService extends projectServiceBaseLib.PlatformProjectServ
 						team: mobileprovision.TeamIdentifier && mobileprovision.TeamIdentifier.length > 0 ? mobileprovision.TeamIdentifier[0] : undefined,
 						uuid: mobileprovision.UUID,
 						name: mobileprovision.Name,
-						identity: mobileprovision.Type === "Development" ? "iPhone Developer" : "iPhone Distribution"
+						identity: !process.env.CODE_SIGN_IDENTITY ? (mobileprovision.Type === "Development" ? "iPhone Developer" : "iPhone Distribution") : process.env.CODE_SIGN_IDENTITY
 					});
 					xcode.save();
 
